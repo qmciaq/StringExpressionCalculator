ceaeffee8a056d655d5e6a9e8734802e249cc0b7
Dominik Iwoła <qmciaq@gmail.com>
2025-08-07 19:44:12 +0200

    Initial spring boot commit

---
+++ src/main/java/pl/domi/calculatortask/CalculatorTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.domi.calculatortask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CalculatorTaskApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/domi/calculatortask/CalculatorTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.domi.calculatortask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTaskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
